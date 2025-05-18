a62b84d6045ee4deea298d7225434a6b885d3bac
harshparmar009 <harshvardhan963628@gmail.com>
2025-05-18 12:53:50 +0530

    env error

--- src/app/api/movies/[id]/route.js
+++ src/app/api/movies/[id]/route.js
@@ -1,20 +1,22 @@
-// app/api/movies/[id]/route.js
-
 import { NextResponse } from "next/server";
 import { connectToDB } from "@/lib/mongoDB";
+import mongoose from "mongoose";
 import Movie from "@/models/Movies";
 
-
-// GET /api/movies/[id]
-export async function GET(request, context) {
+export async function GET(_request, context) {
   const { params } = context;
+
+  if (!mongoose.Types.ObjectId.isValid(params.id)) {
+    return NextResponse.json({ error: "Invalid movie ID" }, { status: 400 });
+  }
+
   try {
     await connectToDB();
 
     const movie = await Movie.findById(params.id);
 
     if (!movie) {
-      return NextResponse.json({ error: 'Movie not found' }, { status: 404 });
+      return NextResponse.json({ error: "Movie not found" }, { status: 404 });
     }
 
     return NextResponse.json(movie, { status: 200 });
